f4a17d052fc8ba813bf3c3288a59d73e87cbdc30
koky-php <dramekoky@gmail.com>
2021-01-05 17:31:12 +0100

    initial commit

---
+++ src/pages/login.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+function LoginPage () {
+    const handleSubmitTeacher = () => {
+        // Submit avec Professeur
+    }
+
+    
+    const handleSubmitAdmin = () => {
+        // Submit avec Administrateur
+    }
+
+    
+    const handleSubmitStudent = () => {
+        // Submit avec Etudiant
+    }
+
+    return (
+        <h2>Mettre la page Login ici.</h2>
+    )
+}
+
+export default LoginPage;
